bbfe1767a0fbd121563ca368510125d030fef913
aditya99933 <adityakumar9933@gmail.com>
2025-01-05 20:36:57 +0530

    minor change

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Statistics({stats}) {
   return (
-    <div className="grid grid-cols-3 gap-4 mb-8 bg-[rgba(184,195,243,0.1)] backdrop-blur-lg border border-indigo-200 rounded-xl p-4 shadow-md">
+    <div className="grid grid-cols-3 gap-4.3 mb-10 bg-[rgba(184,195,243,0.1)] backdrop-blur-lg border border-indigo-200 rounded-xl p-4 shadow-md">
       <div className="text-center">
         <p className="text-gray-400 text-sm">Total Placements</p>
         <p className="text-2xl font-bold text-green-400">{stats.totalPlaced}</p>
       </div>
       <div className="text-center border-x border-indigo-200">
-        <p className="text-gray-200 text-sm">Average Package</p>
+        <p className="text-gray-400 text-sm">Average Package</p>
         <p className="text-2xl font-bold text-green-400">{stats.avgPackage} LPA</p>
       </div>
       <div className="text-center">
