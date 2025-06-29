a23dde2e3f0b564f41e6ffc9936d28d18dd7b312
aditya99933 <adityakumar9933@gmail.com>
2025-06-29 15:04:43 +0530

    Add Google Analytics tracking and update dependencies

--- Frontend/moz-todo-react/src/App.jsx
+++ Frontend/moz-todo-react/src/App.jsx
@@ -1,4 +1,7 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 import Home from "./pages/Home";
 import Jobs from "./pages/Jobs";
 import Placement from "./pages/Placement";
@@ -9,22 +12,33 @@ import JobForm from "./components/JobForm";
 import Campus from "./pages/Campus";
 import ProtectedRoute from "./components/ProtectedRoute";
 
+function AnalyticsTracker() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+  }, [location]);
+  return null;
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/jobs" element={<Jobs />} />
-      <Route path="/placement" element={<Placement />} />
-      <Route path="/charts/:batch" element={<Charts />} />
-      <Route path="/campus" element={<Campus />} />
-      <Route path="/job-details/:id" element={<JobDetailsPage />} />
-      <Route path="/admin-login" element={<AdminLogin />} />
-      <Route path="/add-job" element={
-        <ProtectedRoute>
-          <JobForm />
-        </ProtectedRoute>
-      } />
-    </Routes>
+    <>
+      <AnalyticsTracker />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/placement" element={<Placement />} />
+        <Route path="/charts/:batch" element={<Charts />} />
+        <Route path="/campus" element={<Campus />} />
+        <Route path="/job-details/:id" element={<JobDetailsPage />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/add-job" element={
+          <ProtectedRoute>
+            <JobForm />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </>
   );
 }
 
--- Frontend/moz-todo-react/src/main.jsx
+++ Frontend/moz-todo-react/src/main.jsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactGA from "react-ga4";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+ReactGA.initialize("G-111E8JDP2M");
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-)
+);
