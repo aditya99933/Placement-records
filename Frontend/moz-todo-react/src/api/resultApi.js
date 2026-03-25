711b59924ab93f06a3313daccf436067917fd996
Tanishq Garg <tanishqtanishq56@gmail.com>
2026-03-25 22:27:11 +0530

    API

--- Frontend/moz-todo-react/src/api/resultApi.js
+++ Frontend/moz-todo-react/src/api/resultApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "http://placement-records.onrender.com/api";
 
 export async function initCaptcha() {
   const res = await fetch(`${BASE_URL}/captcha/init`, {
